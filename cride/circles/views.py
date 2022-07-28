73d5e37697dcda79ad7bd542e1a35474564a75d6
Emanuel Romero <e.romero.1989@gmail.com>
2022-07-28 01:05:51 -0300

    add rest_framework and serializers from Circle

---
+++ cride/circles/views.py
@@ -0,0 +1,28 @@
+"""Circles views."""
+
+# Django REST Framework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Models
+from cride.circles.models import Circle
+
+# Serializers
+from cride.circles.serializers import CircleSerializer, CreateCircleSerializer
+
+
+@api_view(['GET'])
+def list_circles(request):
+    """List all circles."""
+    circles = Circle.objects.all()
+    serializer = CircleSerializer(circles, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_circle(request):
+    """Create a new circle."""
+    serializer = CreateCircleSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    circle = serializer.save()
+    return Response(CircleSerializer(circle).data)
