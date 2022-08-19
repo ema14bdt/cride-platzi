6e7c20d9d1663e15cb7a1652e5a60cc3c0779f21
Emanuel Romero <e.romero.1989@gmail.com>
2022-08-19 19:02:50 -0300

    add membership view, serializer  and permission

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -7,11 +7,17 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 # Views
-from cride.circles.views import circles as circles_views
+from .views import circles as circles_views
+from .views import memberships as membership_views
 
 
 router = DefaultRouter()
 router.register(r'circles', circles_views.CircleViewSet, basename='circles')
+router.register(
+    r'circles/(?P<slug_name>[a-zA-Z0-9_-]+)/members',
+    membership_views.MembershipViewSet, 
+    basename='memberships'
+)
 
 urlpatterns = [
     path('', include(router.urls)),
